a7da51e03c2381f41a6b60579284c69e3adc04c6
SJLEE316 <lsj8249@naver.com>
2021-05-07 19:17:43 +0900

    organization(product-show, product-apply)

---
+++ assets/js/apply.js
@@ -0,0 +1,9 @@
+function productClick() {
+  document.querySelector(".product").style.display = "block";
+  document.querySelector(".talent").style.display = "none";
+}
+
+function talentClick() {
+  document.querySelector(".product").style.display = "none";
+  document.querySelector(".talent").style.display = "block";
+}
